4f89656d5f34a9295bef619cc5dca4689c505e0f
Primalr3d <f2016139@goa.bits-pilani.ac.in>
2016-09-12 18:57:45 +0530

    First Commit

---
+++ Python/Inclass/Inclass-1.py
@@ -0,0 +1,9 @@
+for x in range(1,4):
+    if x==1:
+        print "jatin"
+    elif x == 2:
+        print "is a"
+    elif x == 3:
+        print "chutiya"
+    else:
+        print "What the fuck!?!"
