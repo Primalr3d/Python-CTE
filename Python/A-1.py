8f01dbfadb3571170c49d057ec3468398c24e987
Primalr3d <f2016139@goa.bits-pilani.ac.in>
2016-09-12 17:36:29 +0530

    First Commit

---
+++ Python/A-1.py
@@ -0,0 +1,12 @@
+a = "glory"
+print a[0]
+print a[0:2]
+print a[0:3]
+print a[0:4]
+print a[0:5]
+print a[-1]
+print a[4:3:-1]
+print a[4:2:-1]
+print a[4:1:-1]
+print a[4:0:-1]
+print a[4::-1]
---
+++ Python/A-2.py
@@ -0,0 +1,3 @@
+m = input (" Enter the mass ")
+a = input (" Enter the acceleration ")
+print (" The force is ", m*a)
